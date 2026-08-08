a65f07a556a30f86284e4bfdd8cfadc651f3731b
PRATHM <prathmeshravekar2003@gmail.com>
2026-08-08 11:39:54 +0530

    Alignment of hero text & pagination

--- aeronica/aeronica/settings.py
+++ aeronica/aeronica/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
